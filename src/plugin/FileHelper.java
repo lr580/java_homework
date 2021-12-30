89f87f3de0753eb219ed3eb1592ccb8a0d7b6dd7
lr580 <2277379149@qq.com>
2021-12-30 23:27:48 +0800

    做了文件读写类

---
+++ src/plugin/FileHelper.java
@@ -0,0 +1,55 @@
+package plugin;
+
+import java.io.*;
+
+public class FileHelper {// 封装读写文件
+    public static String read(File f) {
+        try {
+            FileReader fr = new FileReader(f);
+            BufferedReader bfr = new BufferedReader(fr);
+            StringBuilder sb = new StringBuilder();
+            String tmp = "";
+            while (null != (tmp = bfr.readLine())) {
+                sb.append(tmp);
+            }
+            bfr.close();
+            fr.close();
+            return sb.toString();
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String read(String f) {
+        return read(new File(f));
+    }
+
+    public static boolean write(String t, File f) {
+        try {
+            FileWriter fw = new FileWriter(f);
+            BufferedWriter bfw = new BufferedWriter(fw);
+            bfw.write(t);
+            bfw.close();
+            fw.close();
+            return true;
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean write(String t, String f) {
+        return write(t, new File(f));
+    }
+
+    public static void main(String[] args) {// 测试用例
+        write("你好吗？a", "a.txt");
+        String t = read("a.txt");
+        System.out.println(t);
+        String v = read("b.txt");
+        if (v == null) {
+            System.out.println("文件不存在");
+        }
+    }
+}
--- src/plugin/Init.java
+++ src/plugin/Init.java
@@ -7,18 +7,35 @@ public class Init {
 
     // 是否已初始化
     public static boolean isInitialized() {
-        boolean init = true;
+        boolean integrity = true;// 是否路径完整
         File f_dir = new File("data/");
         if (!f_dir.exists()) {
             f_dir.mkdir();
-            init = false;
+            integrity = false;
         }
         File f_psw = new File("data/user.txt");
         if (!f_psw.exists()) {
             f_psw.mkdir();
-            init = false;
+            integrity = false;
+        }
+
+        String psw_raw = FileHelper.read(f_psw);
+        if (psw_raw == null) {
+            integrity = false;
+            System.out.println("aa!!");
+            return false;
+        }
+        String psw = Encrypt.decode(psw_raw);
+        
+        if (psw.length() == 0) {
+            System.out.println("a!");
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {// 测试用例
+        // System.out.println(Encrypt.decode("").length());
+        isInitialized();
     }
 }
